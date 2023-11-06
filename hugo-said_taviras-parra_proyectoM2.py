abd6737b5581b4b04223afc5839c3c6bf2e73fb1
hugoparr <145738985+hugoparr@users.noreply.github.com>
2023-11-05 22:30:46 -0600

    Update hugo-said_taviras-parra_proyectoM2.py

--- hugo-said_taviras-parra_proyectoM2.py
+++ hugo-said_taviras-parra_proyectoM2.py
@@ -1,4 +1,3 @@
-#discuklpe no pude hacer qie mabos programas se ejecutaran en paralelo asi que comente uno para dejar que el primero trabaje porfavor comente el primero y descomente el segundo para que pueda funcionar 
 
 
 print('')
@@ -31,26 +30,3 @@ else :
 
 
   
-# # en este programa le pediremos al usuario  que ingrese 2 coordenadas que estran entre los numeros positivos o negativos los cuadrantes los coloque con las 
-# # siguentes relaciones segun entendi yo cuadrante1 (++) cuadrante2 (-+) cuadrante3 (--) cuadrante4 (+-) 
-
-# #en esta parte solicito los datos y los guardo en una variable
-# x = float(input('Ingresa la coordenada de x: ')) 
-# y = float(input('Ingresa la coordenada de y: '))
-
-# # aqui inician las codiciones si el numero de cada variable coincide con los cuadrantes es decir las cuatro combinaciones posibles pues mandara un mensaje 
-# # donde indique en que cuandrante se encuentra en caso de ser ambas variables 0 entonces indicara que es el punto de origen
-# if((x>0) and (y>0)):
-#  print(+x+' y '+y+'se encuentran en el primer cuadrante')
-
-# elif((x<0) and (y>0)):
-#  print(+x+' y '+y+'se encuentran en el segundo cuadrante')
-
-# elif((x<0) and (y<0)):
-#  print(+x+' y '+y+'se encuentran en el tercer cuadrante')
-
-# elif((x>0) and (y<0)):
-#  print(+x+' y '+y+'se encuentran en el cuarto cuadrante')
-
-# else:
-#   print('PUNTO EN EL ORIGEN')
